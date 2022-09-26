fed325f8a4b9d1ca836f097c06ecd156fe85c33d
GabrielRIP <jgrcreations@gmail.com>
2022-09-26 02:03:28 -0500

    Add: lista para ensure_installed

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -4,29 +4,40 @@ if not present then
    return
 end
 
-
-local options = {
-   ensure_installed = {},
-
+treesitter.setup({
+   ensure_installed = { 'lua', 'javascript', 'typescript', 'html', 'json', 'markdown', 'markdown_inline' },
    highlight = {
       enable = true,
-      disable = {
-         'markdown', 'html', 'css', 'scss'
-      },
+      -- disable = { 'markdown', 'html', 'css', 'scss' },
       use_languagetree = true,
    },
-
-   indent = {
+   indent = { -- para porder user indent_blankline
       enable = true,
    },
-   rainbow = {
+   rainbow = { -- modulo de colores en brackets {}, (), []
       enable = true,
       disable = { 'html' }, --list of languages you want to disable the plugin for
       extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
       max_file_lines = nil, -- Do not enable for files with more than n lines, int
       -- colors = {}, -- table of hex strings
       -- termcolors = {} -- table of colour name strings
+   },
+   playground = { -- modulo de prueva de grupos highlight
+      enable = true,
+      disable = {},
+      updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+      persist_queries = false, -- Whether the query persists across vim sessions
+      keybindings = {
+         toggle_query_editor = 'o',
+         toggle_hl_groups = 'i',
+         toggle_injected_languages = 't',
+         toggle_anonymous_nodes = 'a',
+         toggle_language_display = 'I',
+         focus_language = 'f',
+         unfocus_language = 'F',
+         update = 'R',
+         goto_node = '<cr>',
+         show_help = '?',
+      },
    }
-}
-
-treesitter.setup(options)
+})
